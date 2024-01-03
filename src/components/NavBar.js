046e89e255a260c3f3ff53ca0568e24245fa89af
Javi FS <javier.fersal@gmail.com>
2024-01-03 09:49:28 +0000

    Create user context hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,59 @@
-import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import Container from 'react-bootstrap/Container'
-import logo from '../assets/logo.png'
-import styles from '../styles/NavBar.module.css'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
-  return (
-    <Navbar className={styles.NavBar} expand='md' fixed='top'>
-        <Container>
-            <NavLink to="/">
-                <Navbar.Brand>
-                    <img src={logo} alt='logo' height='45' />
-                </Navbar.Brand>
-            </NavLink>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto text-left">
-                    <NavLink
-                        exact
-                        className={styles.NavLink}
-                        activeClassName={styles.Active}
-                        to="/"
-                    >
-                        <i className='fas fa-home'></i>Home
-                    </NavLink>
-                    <NavLink
-                        className={styles.NavLink}
-                        activeClassName={styles.Active}
-                        to="/signin"
-                    >
-                        <i className='fas fa-sign-in-alt'></i>Sign in
-                    </NavLink>
-                    <NavLink
-                        className={styles.NavLink}
-                        activeClassName={styles.Active}
-                        to="/signup"
-                    >
-                        <i className='fas fa-user-plus'></i>Sign up
-                    </NavLink>
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
-  )
-}
+  const currentUser = useCurrentUser();
+  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign in
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signup"
+      >
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
 
-export default NavBar
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
