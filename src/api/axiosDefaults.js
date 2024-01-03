8d4e7cf7abe8219d7ba9c19532d1308bb1dd2c8a
Javi FS <javier.fersal@gmail.com>
2024-01-03 13:35:07 +0000

    handle refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://drf-api-javfs-be8af6f30165.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.baseURL = "https://drf-api-javfs-be8af6f30165.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
